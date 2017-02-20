437cda63aae17b6900584708801585bf6ed5432e
Jelani Thompson <jelani.thompson@outlook.com>
2017-02-20 17:03:13 -0500

    Fin.

--- in_lab/Fraction.h
+++ in_lab/Fraction.h
@@ -5,40 +5,38 @@
 
 using namespace std;
 
-namespace sict{
-   
-   class Fraction{
+namespace sict {
+
+    class Fraction{
     private:
+        int num;			    // Numerator
+        int denom;              // Denominator
 
-   		int num;			    // Numerator
-   		int denom;			  // Denominator
+        int gcd();			  // returns the greatest common divisor of num and denom
+        int max();        // returns the maximum of num and denom
+        int min();        // returns the minimum of num and denom
 
-   		int gcd();			  // returns the greatest common divisor of num and denom
-
-
-      int max();        // returns the maximum of num and denom
-      int min();        // returns the minimum of num and denom
-   
     public:
 
-        void reduce();     // simplifies a Fraction number by dividing the 
-                           // numerator and denominator to their greatest common divisor 
+        void reduce();     // simplifies a Fraction number by dividing the
+        // numerator and denominator to their greatest common divisor
 
         Fraction();                             // default constructor
-      	Fraction(int n , int d=1); 		         // construct n/d as a Fraction number
+        Fraction(int n , int d=1); 		         // construct n/d as a Fraction number
 
-        void display() const;    
+        void display() const;
 
-        bool isEmpty() const;     
-    
+        bool isEmpty() const;
+
         // member operator functions
-
         // TODO: write the prototype of member operator+= function HERE
+        Fraction operator+=(Fraction PlusEqual);
 
         // TODO: write the prototype of member operator+ function HERE
+        Fraction operator+(Fraction Plus);
 
         // TODO: write the prototype of member operator* function HERE
-
-   };
+        Fraction operator*(Fraction Multiply);
+    };
 };
 #endif
